3fe160993e701599419112617d2267757dcbf014
ashtray <ashrayshetty09@gmail.com>
2020-01-16 00:37:51 +0530

    Functionality of popup menu

--- Snapshot/js/app.js
+++ Snapshot/js/app.js
@@ -26,3 +26,23 @@ function smoothScroll(target, duration){
 }
 
 document.getElementById('continue').addEventListener('click',function(){smoothScroll('#main-head', 1500);});
+
+let images_list=document.querySelectorAll('.image');
+images_list.forEach(image=>{
+    image.addEventListener('click', ()=>{
+        let source=image.firstChild.src.split('/');
+        source[source.length-2]="fulls";
+        source=source.join('/');
+
+        var img=document.createElement('IMG');
+        img.src=source;
+        document.getElementById('close').after(img);
+        document.getElementById('image-popup').style.visibility="visible";
+
+        document.getElementById('close').addEventListener('click', ()=>{
+            document.getElementById('image-popup').style.visibility="hidden";
+            document.getElementById('popup-menu').removeChild(img);
+        });
+
+    });
+});
